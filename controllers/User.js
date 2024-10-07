ff5254b128559cffaaf76a5cef6a106c76397c7f
sumit2004tiwari <sumit2004tiwari@gmail.com>
2024-10-07 13:04:23 +0530

    logout api created

--- controllers/User.js
+++ controllers/User.js
@@ -1,231 +1,256 @@
-const bcrypt = require('bcrypt')
-const crypto = require('crypto')
-const nodemailer = require('nodemailer')
-require('dotenv').config()
-const jwt = require('jsonwebtoken');
-
-
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 // dbEntry  for admin
 
 exports.admin = async (req, res) => {
-    const {email, password } = req.body;
+  const { email, password } = req.body;
 
-    console.log(req.body)
+  console.log(req.body);
 
-    if (!email || !password) {
-        return res.status(401).json({
-            success: false,
-            messege: "All field are required...."
-        })
-    }
+  if (!email || !password) {
+    return res.status(401).json({
+      success: false,
+      messege: "All field are required....",
+    });
+  }
 
+  const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+    email,
+  ]);
 
+  if (result.rows.length > 0) {
+    return res.status(400).json({
+      success: false,
+      message: "User Already Registered Please Login.....",
+    });
+  }
 
-    const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+  const hashPassword = await bcrypt.hash(password, 10);
 
-    if (result.rows.length > 0) {
-        return res.status(400).json({
-            success: false,
-            message: "User Already Registered Please Login....."
-        })
-    }
+  try {
+    const query =
+      "INSERT INTO users (firstname, lastname, email, password  ) VALUES ($1,$2,$3,$4) RETURNING *";
+    const value = [firstname, lastname, email, hashPassword];
 
-    const hashPassword = await bcrypt.hash(password, 10);
+    const result = await pool.query(query, value);
+    // console.log(result, ">>>>>>>>>>>>>>>>>>>")
 
-    try {
-        const query = 'INSERT INTO users (firstname, lastname, email, password  ) VALUES ($1,$2,$3,$4) RETURNING *';
-        const value = [firstname, lastname, email, hashPassword];
+    return res.status(200).json({
+      success: true,
+      messege: "Admin created Successfully... ",
+      data: result.rows[0],
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      messege: "Please try again...?",
+    });
+  }
+};
 
-        const result = await pool.query(query, value);
-        // console.log(result, ">>>>>>>>>>>>>>>>>>>")
-
-        return res.status(200).json({
-            success: true,
-            messege: "Admin created Successfully... ",
-            data: result.rows[0]
-
-        })
-    }
-    catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            success: false,
-            messege: "Please try again...?"
-        })
-    }
-
-}
-
-// login 
+// login
 exports.login = async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    console.log(email, password);
+  console.log(email, password);
 
-    if (!email || !password) {
-        return res.status(401).json({
-            success: false,
-            message: "All fields are required."
-        });
+  if (!email || !password) {
+    return res.status(401).json({
+      success: false,
+      message: "All fields are required.",
+    });
+  }
+
+  try {
+    const data = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+    console.log("start>>>>", data, "dattttttttttt");
+
+    if (data.rows.length === 0) {
+      return res.status(402).json({
+        success: false,
+        message: "User not registered. Please sign up.",
+      });
     }
 
-    try {
-        const data = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
-        console.log("start>>>>", data, "dattttttttttt");
+    const user = data.rows[0];
 
-        if (data.rows.length === 0) {
-            return res.status(402).json({
-                success: false,
-                message: "User not registered. Please sign up."
-            });
-        }
+    const matchPassword = await bcrypt.compare(password, user.password);
 
-        const user = data.rows[0];
-
-        const matchPassword = await bcrypt.compare(password, user.password);
-
-        if (!matchPassword) {
-            return res.status(403).json({
-                success: false,
-                message: "Password incorrect."
-            });
-        }
-
-        
-        const payload = {
-            userId: user.id,
-            email: user.email
-        };
-
-        
-        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1m' }); 
-
-       
-        res.cookie('token', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production', 
-            maxAge: 1 * 60 * 1000 
-        });
-
-        return res.status(200).json({
-            success: true,
-            message: "User logged in successfully.",
-            user: {
-                id: user.id,
-                email: user.email
-            }
-        });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            success: false,
-            message: "Please try again."
-        });
+    if (!matchPassword) {
+      return res.status(403).json({
+        success: false,
+        message: "Password incorrect.",
+      });
     }
+
+    const payload = {
+      userId: user.id,
+      email: user.email,
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1m",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 1 * 60 * 1000,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "User logged in successfully.",
+      user: {
+        id: user.id,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Please try again.",
+    });
+  }
 };
 
 // forgotPassword
 exports.forgotPassword = async (req, res) => {
-    const { email } = req.body;
-    console.log("email", email);
+  const { email } = req.body;
+  console.log("email", email);
 
-    if (!email) {
-        return res.status(400).json({
-            success: false,
-            message: "Please enter a valid email"
-        });
+  if (!email) {
+    return res.status(400).json({
+      success: false,
+      message: "Please enter a valid email",
+    });
+  }
+
+  try {
+    const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
     }
 
-    try {
-        const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
-        if (result.rows.length === 0) {
-            return res.status(404).json({
-                success: false,
-                message: "User not found"
-            });
-        }
+    // console.log(email, ">>>>>");
 
-        // console.log(email, ">>>>>");
+    const token = crypto.randomBytes(20).toString("hex");
+    const tokenExpiry = new Date(Date.now() + 2 * 60 * 1000);
 
-    
-        const token = crypto.randomBytes(20).toString('hex');
-        const tokenExpiry = new Date(Date.now() + 2 * 60 * 1000);
+    await pool.query(
+      "UPDATE users SET token = $1, tokenexpire = $2 WHERE email = $3",
+      [token, tokenExpiry, email]
+    );
 
+    const resetLink = `${process.env.API_URL}/api/reset-password?token=${token}`;
 
-       
-        await pool.query('UPDATE users SET token = $1, tokenexpire = $2 WHERE email = $3', [token, tokenExpiry, email]);
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
 
-        const resetLink = `${process.env.API_URL}/api/reset-password?token=${token}`;
+    // email
+    let info = await transporter.sendMail({
+      from: `SquadMinds - by Kamlesh Gupta`,
+      to: email,
+      subject: "Forgot Password Link",
+      html: `<h1>Reset Password Link</h1> <p>Please click on link</p> <a href="${resetLink}">Reset Password</a>`,
+    });
 
-        let transporter = nodemailer.createTransport({
-            host: process.env.MAIL_HOST,
-            auth: {
-                user: process.env.MAIL_USER,
-                pass: process.env.MAIL_PASS,
-            }
-        });
-
-
-        // email
-        let info = await transporter.sendMail({
-            from: `SquadMinds - by Kamlesh Gupta`,
-            to: email,
-            subject: 'Forgot Password Link',
-            html: `<h1>Reset Password Link</h1> <p>Please click on link</p> <a href="${resetLink}">Reset Password</a>`
-        });
-
-
-        return res.status(200).json({
-            success: true,
-            message: "Password reset mail sent successfully"
-        });
-
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({
-            success: false,
-            message: "Please try again"
-        });
-    }
+    return res.status(200).json({
+      success: true,
+      message: "Password reset mail sent successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Please try again",
+    });
+  }
 };
 
 // resetPassword
 exports.resetPassword = async (req, res) => {
+  const { newPassword } = req.body;
+  const token = req.query.token;
 
-    const {newPassword}  = req.body;
-    const token  = req.query.token;
+  if (!token || !newPassword) {
+    return res.status(400).json({
+      success: false,
+      message: "all field are required.",
+    });
+  }
 
-    if (!token || !newPassword) {
-        return res.status(400).json({
-            success: false,
-            message: "all field are required."
-        });
+  try {
+    const result = await pool.query("SELECT * FROM users WHERE token = $1", [
+      token,
+    ]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "token expire",
+      });
     }
 
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    await pool.query(
+      "UPDATE users SET password = $1, token = NULL WHERE token = $2",
+      [hashedPassword, token]
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Password  reset successfully.",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Password not reset please try again...",
+    });
+  }
+};
+
+exports.logoutUser = async (req, res) => {
+    const { token } = req.body;
+  
     try {
-        const result = await pool.query('SELECT * FROM users WHERE token = $1', [token]);
-
-        if (result.rows.length === 0) {
-            return res.status(404).json({
-                success: false,
-                message: "token expire"
-            });
-        }
-
-        const hashedPassword = await bcrypt.hash(newPassword, 10);
-
-        await pool.query('UPDATE users SET password = $1, token = NULL WHERE token = $2', [hashedPassword, token]);
-
-        return res.status(200).json({
-            success: true,
-            message: "Password  reset successfully."
-        });
+      if (!token) {
+        return res.status(400).json({ message: "token not found" });
+      }
+      const queryCheck = `SELECT * FROM users WHERE logintoken = $1`;
+      const tokencheck = await pool.query(queryCheck, [token]);
+  
+      if (!tokencheck) {
+        return res.status(400).json({ message: "token is invalid or expire" });
+      }
+  
+      const query = `UPDATE users Set logintoken = NULL  WHERE logintoken = $1`;
+      await pool.query(query, [token]);
+      res.clearCookie("token");
+      return res.status(200).json("Logout successfully");
+  
     } catch (error) {
-        console.error( error);
-        return res.status(500).json({
-            success: false,
-            message: "Password not reset please try again..."
-        });
+      console.error("Error logging out ", error);
+      return res.status(400).json("somthing went wrong during logout");
     }
-}
+  };
